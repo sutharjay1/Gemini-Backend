63fb33dc365f49a30281f08543489549a94d9f86
Jay Suthar <suthajay3635@gmail.com>
2024-04-21 14:54:16 +0530

    GeminiX

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -6,6 +6,9 @@ const handleUserRegister = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
+    if (!name || !email || !password)
+      return res.status(400).json({ message: 'All fields are required' });
+
     const existingUser = await User.findOne({ email });
 
     if (existingUser)
